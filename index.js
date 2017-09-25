1c18a6ded1d766789653fc33528babf40aa3300d
Bruce Wong <xenovia@Bruces-MacBook-Pro.local>
2017-09-24 23:44:25 -0700

    fixed port env variable

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db = mongoose();
 const app = express();
 const passport = passportConfig();
 
-const port = process.env.NODE_ENV.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log('Server running at http://localhost:' + port);
