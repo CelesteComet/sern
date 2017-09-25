9c01190be83a95b31581986e6e85c9a304c97f4a
Bruce Wong <xenovia@Bruces-MacBook-Pro.local>
2017-09-24 23:54:18 -0700

    fixed port envawdwadle

--- config/keys/prod.js
+++ config/keys/prod.js
@@ -1,3 +1,7 @@
 module.exports = {
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_MAPS_KEY: process.env.GOOGLE_MAPS_KEY,
+  MONGO_URI: process.env.MONGO_URI
 }
